a5efcccd8a7e16b2827fbe5d437d18c2a7318251
Leonardo Henrique <leonardo.machado@fontespromotora.com.br>
2023-08-01 20:44:33 -0300

    refactor(src/app/services): Changes lib axios to native angular lib http

--- src/app/Services/products.service.ts
+++ src/app/Services/products.service.ts
@@ -1,20 +1,19 @@
-import { Injectable } from '@angular/core';
-import { Product } from '../Types/product.type';
-import axios from 'axios';
+import { Injectable, inject } from '@angular/core';
+import { Product } from '../Types/';
 import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProductService {
   readonly apiUrl = environment.apiURL;
-
-  api = axios.create({ baseURL: this.apiUrl });
-
-  async getAllProducts(): Promise<Product[]> {
-    const { data } = await this.api.get('/products');
-    return data ?? [];
-  }
+  http: HttpClient = inject(HttpClient);
 
   constructor() {}
+
+  getAllProducts(): Observable<Product[]> {
+    return this.http.get<Product[]>(this.apiUrl + '/products');
+  }
 }
