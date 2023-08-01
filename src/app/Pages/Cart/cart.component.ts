09de27282f1309a19600c219c904c85c06b6f535
Leonardo Henrique <leonardo.machado@fontespromotora.com.br>
2023-08-01 20:52:28 -0300

    refactor(src/app/pages/cart): Refactors cart page

--- src/app/Pages/Cart/cart.component.ts
+++ src/app/Pages/Cart/cart.component.ts
@@ -1,21 +1,31 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { BaseComponent } from 'src/app/Components/Base/base.component';
+import { CartItemComponent } from 'src/app/Components/CartItem/cartItem.component';
 import { HeaderComponent } from 'src/app/Components/Header/header.component';
+import { State } from 'src/app/Services/state.service';
+import { CartItem } from 'src/app/Types';
 
 @Component({
   selector: 'app-cart',
   standalone: true,
-  imports: [CommonModule, HeaderComponent],
+  imports: [CommonModule, HeaderComponent, CartItemComponent],
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.css'],
 })
-export class CartComponent {
-  // productService: ProductService = inject(ProductService);
-  // async getAllProducts() {
-  //   const products = await this.productService.getAllProducts();
-  //   console.log(products);
-  // }
-  // constructor() {
-  //   this.getAllProducts();
-  // }
+export class CartComponent extends BaseComponent {
+  state: State = inject(State);
+  carItems: CartItem[] = [];
+
+  constructor() {
+    super();
+
+    this.state.cartItemsState
+      .pipe(this.unsubOnDestroy())
+      .subscribe((cartItems) => (this.carItems = [...cartItems]));
+  }
+
+  ngOnInit() {
+    this.state.setSearch('');
+  }
 }
